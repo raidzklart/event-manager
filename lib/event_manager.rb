903ddc6542b626af2212454267abaa43d1e0a0dc
Thomas Christopher <thomas.j.christopher@gmail.com>
2019-08-31 22:37:10 +0100

    Save letters to output directory

--- lib/event_manager.rb
+++ lib/event_manager.rb
@@ -21,6 +21,16 @@ def legislators_by_zipcode(zip)
   end
 end
 
+def save_thank_you_letters(id,form_letter)
+  Dir.mkdir("output") unless Dir.exists?("output")
+
+  filename = "output/thanks_#{id}.html"
+
+  File.open(filename,'w') do |file|
+    file.puts form_letter
+  end
+end
+
 puts "\n\nEventManager Initialized!\n\n"
 
 template_letter = File.read "form_letter.erb"
@@ -28,13 +38,16 @@ erb_template = ERB.new template_letter
 
 contents = CSV.open "event_attendees.csv", headers: true, header_converters: :symbol
 contents.each do |row|
+  id = row[0]
   name = row[:first_name]
-  zipcode = row[:zipcode]
-
   zipcode = clean_zipcode(row[:zipcode])
 
   legislators = legislators_by_zipcode(zipcode)
 
   form_letter = erb_template.result(binding)
-  puts form_letter
+  Dir.mkdir("output") unless Dir.exists? "output"
+
+  filename = "output/thanks_#{id}.html"
+
+  save_thank_you_letters(id,form_letter)
 end
